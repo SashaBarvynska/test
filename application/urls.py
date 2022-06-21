e079d6432af41836b32890d57c5bc256ccab2066
Sasha Barvynska <alexbarvynska@gmail.com>
2022-06-22 00:06:21 +0300

    Changed views methods

--- application/urls.py
+++ application/urls.py
@@ -2,20 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('add/', views.add, name='add'),
-  path('add/addrecord/', views.addrecord, name='addrecord'),
-  path('delete/<int:id>', views.delete, name='delete'),
-  path('member/update/<int:id>', views.update, name='update'),
-  path('member/update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
-  path('pets', views.pets, name='pets'),
-  path('addpets/', views.addpets, name='addpets'),
-  path('addpets/addrecordpet/', views.addrecordpet, name='addrecordpet'),
-  path('member/<int:member_id>/adopt', views.adopt, name='adopt'),
-  path('member/<int:member_id>/pet/<int:pet_id>/add', views.adoptpet, name='adoptpet'),
-  path('member/<int:id>', views.getDetails, name='getDetails'),
-  path('deletepet/<int:id>', views.deletepet, name='deletepet'),
-  path('updatepet/<int:id>', views.updatepet, name='updatepet'),
-  path('updatepet/updaterecordpet/<int:id>', views.updaterecordpet, name='updaterecordpet'),
-  path('pet/<int:pet_id>/remove', views.removepet, name='removepet'),
+  path('members', views.handle_all_members, name='handle_all_members'),#Get
+  path('members/<int:id>', views.handle_member, name='handle_member'),
+
+  path('pets', views.handle_all_pets, name='handle_all_pets'),
+  path('pets/<int:id>', views.handle_pet, name='handle_pet'),
+
+  path('pets/<int:pet_id>/add', views.adopt_pet, name='adopt_pet'),
+  path('pets/<int:pet_id>/remove', views.remove_pet, name='remove_pet'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,144 +1,183 @@
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from turtle import reset
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpRequest, QueryDict
 from django.template import loader
 from django.urls import reverse
 from .models import Members, Pets
 
-# members
-def index(request):
-  mymembers = Members.objects.all().values()
-  for mymember in mymembers:
-    mymember['fullname'] = mymember['firstname'] + ' ' + mymember['lastname']
-      
-
-  template = loader.get_template('members/index.html')
-
-  context = {
-    'mymembers': list(mymembers),
-  }
-  return JsonResponse(context)
-  
-def add(request):
-  template = loader.get_template('members/add.html')
-  return HttpResponse(template.render({}, request))
-  
-def addrecord(request):
-  first = request.POST['first']
-  
-  last = request.POST['last']
-  age = request.POST['age']
-  member = Members(firstname=first, lastname=last, age=age)
-  member.save()
-  return HttpResponseRedirect(reverse('index'))
-
-def delete(request, id):
-  member = Members.objects.get(id=id)
-  member.delete()
-  return HttpResponseRedirect(reverse('index'))
-
-def update(request, id):
-    mymember = Members.objects.get(id=id)
-    template = loader.get_template('members/update.html')
-    context = {
-    'mymember': mymember,
-    }
-    return HttpResponse(template.render(context, request))
-
-def updaterecord(request, id):
-  first = request.POST['first']
-  last = request.POST['last']
-  age = request.POST['age']
-  member = Members.objects.get(id=id)
-  member.firstname = first
-  member.lastname = last
-  member.age = age
-  member.save()
-  return HttpResponseRedirect(reverse('index'))
-
-def getDetails(request, id):
-    member = Members.objects.get(id=id)
-
-    try:
-        mypets = Pets.objects.filter(member_id=id)
-        # print(type(mypets), "mypets*****************************************************************")
-
-    except Pets.DoesNotExist:
-        mypets = []
-        
-    template = loader.get_template('members/details.html')
-    context = {
-    'member': member,
-    'mypets': mypets,
-    }
-    return HttpResponse(template.render(context, request))
-    
-
 # pets
-def pets(request):
-    mypets = Pets.objects.all().values()
-    template = loader.get_template('pets/index.html')
-    context = {
-    'mypets': mypets, 
-    'isAdopt': False,
-    }
-    return HttpResponse(template.render(context, request))
-
-def addpets(request):
-  template = loader.get_template('pets/addpets.html')
-  return HttpResponse(template.render({}, request))
-  
-
-def addrecordpet(request):
-  name = request.POST['name']
-  type = request.POST['type']
-  gender = request.POST['gender']
-  pet = Pets(name=name, type=type, gender=gender)
-  pet.save()
-  return HttpResponseRedirect(reverse('pets'))
-
-def adopt(request, member_id):
-    mypets = Pets.objects.all().values()
-    template = loader.get_template('pets/index.html')
-    context = {
-    'mypets': mypets, 
-    'isAdopt': True,
-    'member_id': member_id,
-    }
-    return HttpResponse(template.render(context, request))
-
-def adoptpet(request, member_id, pet_id):
+def adopt_pet(request, pet_id):#PUT 
+    put = QueryDict(request.body)
+    member_id = put.get('member_id')
     pet = Pets.objects.get(id=pet_id)
     pet.member_id = member_id
     pet.save()
-    return HttpResponseRedirect(reverse('pets'))
+    return JsonResponse({
+    'adopted_pet':{
+      'id': pet.pk,
+      'name': pet.name, 
+      'type': pet.type, 
+      'gender': pet.gender,
+      'member_id': pet.member_id,
+      },
+    })
 
-def deletepet(request, id):
-  pet = Pets.objects.get(id=id)
-  pet.delete()
-  return HttpResponseRedirect(reverse('index'))
-
-def updatepet(request, id):
-    mypet = Pets.objects.get(id=id)
-    template = loader.get_template('pets/updatepet.html')
-    context = {
-    'mypet': mypet,
-    }
-    return HttpResponse(template.render(context, request))
-
-def updaterecordpet(request, id):
-  name = request.POST['name']
-  type = request.POST.get('type', False)
-  gender = request.POST['gender']
-  pet = Pets.objects.get(id=id)
-  pet.name = name
-  pet.type = type
-  pet.gender = gender
+def remove_pet(request, pet_id):#PUT 
+  pet = Pets.objects.get(id=pet_id)
+  pet.member_id = None
   pet.save()
-  return HttpResponseRedirect(reverse('index'))
+  return JsonResponse({
+    'removed_pet':{
+      'id': pet.pk,
+      'name': pet.name, 
+      'type': pet.type, 
+      'gender': pet.gender,
+      'member_id': pet.member_id,
+      },
+    })
 
-def removepet(request, pet_id):
- #  mypet = Pets.objects.get(name column = value column)
-  mypet = Pets.objects.get(id=pet_id)
-  mypet.member_id = None
-  mypet.save()
-  return HttpResponseRedirect(reverse('index'))
+
+
+def handle_member(request: HttpRequest, id):
+  if request.method == "GET":
+    member = Members.objects.get(id=id)
+
+    try:
+        mypets = Pets.objects.filter(member_id=id).values()
+    except Pets.DoesNotExist:
+        mypets = []
+        
+    return JsonResponse({
+    'member': {  
+        'id': member.id, 
+        'firstname': member.firstname, 
+        'lastname': member.lastname, 
+        'age': member.age
+      },
+    'mypets': list(mypets),
+    })
+
+  if request.method == 'PUT':
+    put = QueryDict(request.body)
+    first = put.get('first')
+    last = put.get('last')
+    age = put.get('age')
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.age = age
+    member.save()
+    return JsonResponse({
+    'updated_member': {
+      'id': member.pk,
+      'firstname': first, 
+      'lastname': last, 
+      'age': age
+      },
+    })
+
+  if request.method == 'DELETE':
+    member = Members.objects.get(id=id)
+    member.delete()
+    return JsonResponse({
+    'deleted_member': {
+      'id': member.pk,
+      'firstname': first, 
+      'lastname': last, 
+      'age': age
+      },
+    })
+
+def handle_all_members(request: HttpRequest):
+  if request.method == 'GET':
+    mymembers = Members.objects.all().values()
+
+    for mymember in mymembers:
+      mymember['fullname'] = mymember['firstname'] + ' ' + mymember['lastname']
+        
+    return JsonResponse({
+      'mymembers': list(mymembers),
+    })
+
+  if request.method == 'POST':
+    first = request.POST['first']
+    last = request.POST['last']
+    age = request.POST['age']
+    member = Members(firstname=first, lastname=last, age=age)
+    member.save()
+    return JsonResponse({
+      'updated_member': {'id': member.pk, 'firstname': first, 'lastname': last, 'age': age},
+    })
+
+
+
+def handle_pet(request: HttpRequest, id):
+
+  if request.method == "GET":
+    pet = Pets.objects.get(id=id)
+    return JsonResponse({
+    'pet': {
+      'id': pet.pk,
+      'name': pet.name, 
+      'type': pet.type, 
+      'gender':pet.gender,
+      'member_id': pet.member_id,
+      },
+    })
+
+  if request.method == 'PUT':
+    put = QueryDict(request.body)
+    name = put.get('name')
+    type = put.get('type')
+    gender = put.get('gender')
+    pet = Pets.objects.get(id=id)
+    pet.name = name
+    pet.type = type
+    pet.gender = gender
+    pet.save()
+    return JsonResponse({
+    'updated_pet': {
+      'id': pet.pk,
+      'name': pet.name, 
+      'type': pet.type, 
+      'gender':pet.gender,
+      'member_id': pet.member_id,
+      },
+    })
+
+  if request.method == 'DELETE':
+    pet = Pets.objects.get(id=id)
+    pet.delete()
+    return JsonResponse({
+    'deleted_pet': {
+      'id': pet.pk,
+      'name': pet.name, 
+      'type': pet.type, 
+      'gender': pet.gender,
+      'member_id': pet.member_id,
+      },
+    })
+
+
+def handle_all_pets(request: HttpRequest):
+
+  if request.method == 'POST':
+    name = request.POST['name']
+    type = request.POST['type']
+    gender = request.POST['gender']
+    pet = Pets(name=name, type=type, gender=gender)
+    pet.save()
+    return JsonResponse({
+      'updated_pet': {
+        'id': pet.pk, 
+        'name': pet.name, 
+        'type': pet.type, 
+        'gender': pet.gender},
+        'member_id': pet.member_id,
+    })
+
+  if request.method == 'GET':
+    mypets = Pets.objects.all().values()
+    return JsonResponse({
+    'mypets': list(mypets), 
+    })
 
