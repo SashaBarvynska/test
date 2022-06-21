27d79dfaeba0708f82ba389bd30585f6e090b883
Andrew <47723945+AndrewLebowski@users.noreply.github.com>
2022-06-21 12:47:30 +0300

    it's me

--- application/models.py
+++ application/models.py
@@ -7,7 +7,7 @@ class Members(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   age = models.SmallIntegerField(null=True)
-  
+  #
 
 class Pets(models.Model):
   member = models.ForeignKey(Members, on_delete=models.CASCADE, null=True)
