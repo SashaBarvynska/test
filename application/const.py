c8b8ffb90a365013c0f84babcdf38ab84577a72e
Sasha Barvynska <alexbarvynska@gmail.com>
2022-06-26 00:00:11 +0300

    add wallet and address models

---
+++ application/const.py
@@ -0,0 +1,30 @@
+FIRSTNAMES = [
+    "Sasha",
+    "Andrew",
+    "Ura",
+    "Eva",
+    "Polina",
+    "Vika",
+    "Vitya",
+    "Kostya",
+]
+LASTNAMES = [
+    "Melnyk",
+    "Kovalchuk",
+    "Bondarenko",
+    "Kravchuk",
+    "Shevchenko",
+    "Tymoshenko",
+]
+NAMES = ["Markiza", "Amur", "Tarzan", "Oskar", "Charly", "Karandash", "Milka"]
+TYPES = ["cat", "dog", "cavy", "lizard", "parrot", "fish"]
+GENDERS = ["male", "female"]
+CITIES = [
+            "Melitopol",
+            "Rivne",
+            "Odessa",
+            "Dnipro",
+            "London",
+            "Istambul",
+            "New York",
+        ]
